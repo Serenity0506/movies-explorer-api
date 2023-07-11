8aa0b8f204160548dedd2951ac088ef20c9f5ec1
Liubov Gayduk <serenity0506@yandex.ru>
2023-07-11 15:48:05 +0300

    fixed limiter

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const limiter = require('./middlewares/limiter');
+const { limiter } = require('./middlewares/limiter');
 const allRouters = require('./routes/allRouters');
 const { handleExceptions } = require('./middlewares/errorMiddleware');
 
